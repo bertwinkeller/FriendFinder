c3fdedd18eed26a08e5d5ea42d54ec44382e8328
Bert Keller <bertwinkeller@gmail.com>
2019-11-17 18:21:02 -0800

    html pages up and running

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ const apiRoutes = require('./app/routing/apiRoutes.js')
 const htmlRoutes = require('./app/routing/htmlRoutes.js')
 
 
-
-
 // sets port
 const app = express()
 const PORT = process.env.PORT || 8080;
+
 
 // declare middleware
 app.use(bodyParser.json());
@@ -21,8 +20,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use('/static', express.static(path.join(__dirname, 'app/public')))
-
-
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
